8d99ac048a03de6ab4c1516352324fbbc485388c
jongoldbergnfp <jon.goldberg@nursefamilypartnership.org>
2017-08-25 21:27:30 +0000

    config issues fixed

--- post/assessment/post_assessment.py
+++ post/assessment/post_assessment.py
@@ -13,6 +13,8 @@ def lambda_handler(event, context):
    
     if environment == 'Beta':
        import rds_proto_config
+       #keyId  = rds_proto_config.keyId
+       #sKeyId  = rds_proto_config.sKeyId
     elif environment == 'dev':
        import rds_dev_config
     else:
@@ -21,24 +23,16 @@ def lambda_handler(event, context):
     keyId = "AKIAJ76RR3BYTOPWPFUQ"
     sKeyId="TiHi8LVgSprJHt5H44ijedaGuZECFz0sRJ8dARvA"
     #Connect to S3 with access credentials
-    #conn = boto.connect_s3(keyId,sKeyId)
-    #bucket = conn.get_bucket('api-nfp')
-    #from boto.s3.key import Key
- 
+    conn = boto.connect_s3(keyId,sKeyId)
+    bucket = conn.get_bucket('api-nfp')
+    from boto.s3.key import Key
+
     #Get the Key object of the bucket
-    #k = Key(bucket)
-    
+    k = Key(bucket)
+
     #Crete a new key with id as the name of the file
-    #k.key=reqid+".assessment.apigateway.json"
-    
+    k.key=reqid+".assessment.apigateway.json"
+
     #Upload the file
-    #result = k.set_contents_from_string(rawdata)
-    #result contains the size of the file uploaded
-    
-    #print json_data
-    #print data["Ethnicity"]
-    
-    #for element in data['Race']:
-    #    print element
-    
-    #print json.dumps(data, sort_keys=True, indent=4)
+    result = k.set_contents_from_string(rawdata)
+
--- post/assessment/rds_dev_config.py
+++ post/assessment/rds_dev_config.py
@@ -5,3 +5,7 @@ db_password = "NFP!2017!"
 db_name = "dev"
 db_endpoint = "jdg-proto-type.ccf0v3myj6k1.us-west-2.rds.amazonaws.com"
 
+#S3 Config
+#keyId = "AKIAJ76RR3BYTOPWPFUQ"
+#sKeyId="TiHi8LVgSprJHt5H44ijedaGuZECFz0sRJ8dARvA"
+
