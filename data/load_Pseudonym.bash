519c45de7ca255c21c701b9be4db177736a55038
jongoldbergnfp <jon.goldberg@nursefamilypartnership.org>
2017-08-25 14:33:55 +0000

    added script to load the pseudonym table

--- data/load_Pseudonym.bash
+++ data/load_Pseudonym.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-mysql --user="master" --password='NFP!2017!' --host=jdg-proto-type.ccf0v3myj6k1.us-west-2.rds.amazonaws.com << EOF
+mysql --user="master" --password='NFP!2017!' --host=jdg-proto-type.ccf0v3myj6k1.us-west-2.rds.amazonaws.com --database=proto << EOF
+source create_pseudonym.sql
+
+DELETE FROM pseudonym;
+
+LOAD DATA LOCAL INFILE 'Pseudonym.csv'
+INTO TABLE pseudonym
+	FIELDS TERMINATED BY ','
+		OPTIONALLY ENCLOSED BY '"'
+	LINES  TERMINATED BY '\n'
+(pseudonym_name, default_question)
+SET pseudonym_id = NULL,
+    start_date = str_to_date('01/01/2000','%m/%d/%Y');
+
 exit
 EOF
