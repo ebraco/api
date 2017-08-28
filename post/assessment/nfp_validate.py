80ec5dc83845be7249d6b26f7d49a8e5dd17a460
jongoldbergnfp <jon.goldberg@nursefamilypartnership.org>
2017-08-28 16:38:52 +0000

    Validation working library!  :-)

--- post/assessment/nfp_validate.py
+++ post/assessment/nfp_validate.py
@@ -3,17 +3,7 @@
 def validate_pseudonym(conn_in, pseudonym_name_in):
 
     with conn_in.cursor() as cur:
-       cur.execute("select pseudonym_name as Pseudonym from pseudonym = %s", (pseudonym_name_in))
-       row_headers=[x[0] for x in cur.description]
-       rv = cur.fetchall()
-       json_data=[]
-       for result in rv:
-          json_data.append(dict(zip(row_headers,result)))
-
-       if qs == "error":
-          raise Exception('"NFP-Error-Code:400059"')
-       else:
-          return json_data
+       cur.execute('select pseudonym_name from pseudonym where pseudonym_name = %s', (pseudonym_name_in))
 
     return
 
